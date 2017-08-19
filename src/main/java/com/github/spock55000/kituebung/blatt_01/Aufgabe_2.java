a81e15f4013bf76aa3d5312a9deb2b180881c2e5
Spock55000 <spock45000-7@yahoo.de>
2017-08-19 20:35:45 +0200

    Teil E
    Runden, wie soll das ohne Math gehen?

--- src/main/java/com/github/spock55000/kituebung/blatt_01/Aufgabe_2.java
+++ src/main/java/com/github/spock55000/kituebung/blatt_01/Aufgabe_2.java
@@ -8,24 +8,36 @@ public class Aufgabe_2 {
 
     public void run(){
         System.out.println("Jo alda, herzlich willkommen hier, lan");
-        int x = reader.readInt("Würfelseitenlänge x in cm: ");
+        double x = reader.readInt("Würfelseitenlänge x in cm: ");
         int m = reader.readInt("Würfelmaterial m auswählen: ");
-        int plastikdichte = 5;
-        int holzdichte = 15;
-        int eisendichte = 25;
-        int restdichte = 10000;
+        int plastikdichte = 920; //korrekter Wert in kg/m³
+        int holzdichte = 690; //korrekter Wert in kg/m³
+        int eisendichte = 7874; //korrekter Wert in kg/m³
+        int bleidichte = 11342; //korrekter Wert in kg/m³
         int roh = 0;
         switch (m){
             case 1: roh = plastikdichte;
+            System.out.println("Sie haben Plastik ausgewählt");
             break;
             case 2: roh = holzdichte;
+            System.out.println("Sie haben Holz ausgewählt");
             break;
-            case 3: roh = restdichte;
+            case 3: roh = eisendichte;
+            System.out.println("Sie haben Eisen ausgewählt");
             break;
-            default: roh = restdichte;
+            default: roh = bleidichte;
+            System.out.println("Sie haben Blei ausgewählt");
         }
-        float gewicht = (((x*x*x)/1000)*roh)/1000;
+        double gewicht = ((((x*x*x)/1000000)*roh));
+        // Teil D - Kraft F die der Würfel auf den Boden ausübt.
+        double erdebeschl = 9.813; //genauer Wert für BaWü
+        double gewichtskraft = gewicht*erdebeschl;
+
+
+        //Teil E - Runden und Ausgabe
         System.out.println("Würfelgewicht = " +gewicht + " Kg");
+        System.out.println("Gewichtskraft F: "+gewichtskraft+" N");
+
     }
 
 
