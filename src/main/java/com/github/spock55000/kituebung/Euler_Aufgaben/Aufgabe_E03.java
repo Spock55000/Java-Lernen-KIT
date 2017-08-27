057cf547a8279a78cf157a01a1bc7f2c49e5fd3a
Spock55000 <spock45000-7@yahoo.de>
2017-08-27 05:15:31 +0200

    AE03 Aufgabe gelöst

--- src/main/java/com/github/spock55000/kituebung/Euler_Aufgaben/Aufgabe_E03.java
+++ src/main/java/com/github/spock55000/kituebung/Euler_Aufgaben/Aufgabe_E03.java
@@ -4,15 +4,19 @@ import com.github.spock55000.kituebung.LineReader;
 
 public class Aufgabe_E03 {
     private LineReader reader = new LineReader();
-    public void run(){
-        double p = 600851475143.0;
-        int i = 1;
-        int x;
+    public void run() {
+        long p = 600851475143L;
+        for (long j = 2; j <= p; j++) {
+            if ((p % j) == 0) {
+                System.out.println(j);
+
+                p /= j;
 
 
-
-
-
-
+            }
+        }
     }
+
+
 }
+
