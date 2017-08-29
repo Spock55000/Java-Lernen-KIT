5a94a606bef95f694adde85a2f731dd208bc5543
Spock55000 <spock45000-7@yahoo.de>
2017-08-29 02:41:00 +0200

    Binäre Suche fehler hä?

---
+++ src/main/java/com/github/spock55000/kituebung/Folien/Binaere_Suche.java
@@ -0,0 +1,17 @@
+package com.github.spock55000.kituebung.Folien;
+
+public class Binaere_Suche {
+    public int[] suchFeld = {1,2,3,4,6,7,8,9,10,35,62}; //Feld geordnet!
+    public static int findeIndexBinaerRekursiv(int indexAnfang, int indexEnde, int suchNummer) {
+        if (indexAnfang > indexEnde) { return -1; }
+        int indexMitte = indexAnfang + ((indexEnde - indexAnfang) / 2);
+        if (suchNummer > suchFeld[indexMitte]) {
+            return findeIndexBinaerRekursiv(indexMitte + 1, indexEnde,suchNummer);
+        } else if (suchNummer < suchFeld[indexMitte]) {
+            return findeIndexBinaerRekursiv(indexAnfang, indexMitte - 1,suchNummer);
+        }
+        return indexMitte;
+
+        }
+    }
+}
