d287d6f90b96e3bd7fc761d14c70f6ae2f6214af
Spock55000 <spock45000-7@yahoo.de>
2017-08-31 20:11:31 +0200

    Sortierung

---
+++ src/main/java/com/github/spock55000/Euler_Aufgaben/Aufgabe_E05.java
@@ -0,0 +1,6 @@
+package com.github.spock55000.Euler_Aufgaben;
+
+public class Aufgabe_E05 {
+
+    }
+
--- src/main/java/com/github/spock55000/kituebung/Euler_Aufgaben/Aufgabe_E05.java
+++
@@ -1,8 +0,0 @@
-package com.github.spock55000.kituebung.Euler_Aufgaben;
-
-import com.github.spock55000.kituebung.LineReader;
-
-public class Aufgabe_E05 {
-
-    }
-
