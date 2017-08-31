baeaef05d7a3d2542257923ce5fd3f17d3c3b780
Spock55000 <spock45000-7@yahoo.de>
2017-08-31 20:02:43 +0200

    Kangaroo neu

---
+++ src/main/java/com/github/spock55000/com/training/buch/Kangeroo.java
@@ -0,0 +1,39 @@
+package com.github.spock55000.com.training.buch;
+
+public class Kangeroo {
+    double schlagkraft;
+    double sprungweite;
+
+    void trainieren(){
+        System.out.println("Das Känguru trainiert!");
+        double r1 = Math.random();
+        schlagkraft += r1;
+        System.out.println("Das Känguru hat nun eine Schlagkraft von " + schlagkraft);
+        double r2 = Math.random();
+        sprungweite += r2;
+        System.out.println("Es springt nun " + sprungweite + " Meter weit");
+    }
+
+    boolean boxen(){
+        int r = (int)(Math.random() * 10);
+        if (r <5) {
+            System.out.println("Das Känguru haut daneben!");
+            return false;
+        } else {
+                System.out.println("Das Känguru trifft!");
+                return true;
+            }
+        }
+
+
+
+
+    double springen(){
+        double sprung = 0;
+        do {
+            sprung = (int) (Math.random() * 10);
+        } while (sprung > sprungweite);
+        System.out.println("Das Känguru springt " + sprung + " Meter von maximal " + sprungweite);
+        return sprung;
+    }
+}
---
+++ src/main/java/com/github/spock55000/com/training/buch/KangerooTest.java
@@ -0,0 +1,11 @@
+package com.github.spock55000.com.training.buch;
+
+public class KangerooTest {
+    public static void main(String[] args){
+        Kangeroo katrin;
+        katrin = new Kangeroo();
+        katrin.trainieren();
+        katrin.springen();
+        katrin.boxen();
+    }
+}
