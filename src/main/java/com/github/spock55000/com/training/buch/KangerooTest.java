d1c2c6d03f01a0daf9950e1854dd73ec662ff984
Spock55000 <spock45000-7@yahoo.de>
2017-08-31 20:24:01 +0200

    Sortierung

--- src/main/java/com/github/spock55000/com/training/buch/KangerooTest.java
+++ src/main/java/com/github/spock55000/com/training/buch/KangerooTest.java
@@ -2,10 +2,19 @@ package com.github.spock55000.com.training.buch;
 
 public class KangerooTest {
     public static void main(String[] args){
-        Kangeroo katrin;
-        katrin = new Kangeroo();
-        katrin.trainieren();
-        katrin.springen();
-        katrin.boxen();
+        Kangeroo elsa;
+        elsa = new Kangeroo();
+        for (int i = 0; i < 3; i++){
+            elsa.trainieren();
+        }
+        elsa.springen();
+        elsa.boxen();
+        Kangeroo gerd;
+        gerd = new Kangeroo();
+        for (int i = 0; i < 3; i++){
+            gerd.trainieren();
+        }
+        gerd.springen();
+        gerd.boxen();
     }
 }
